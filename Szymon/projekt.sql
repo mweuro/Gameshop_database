6ad61f08841efd1db080c119145fa07e98c967d8
Szymon Malec <77552397+Szymex49@users.noreply.github.com>
2023-06-13 22:54:55 +0200

    update

---
+++ Szymon/projekt.sql
@@ -0,0 +1,16 @@
+
+
+
+select * from games_for_sale;
+
+
+
+
+drop table competition_results;
+drop table competition;
+drop table games_for_sale;
+drop table games_to_rent;
+drop table staff;
+drop table customers;
+drop table sale;
+drop table rental;
