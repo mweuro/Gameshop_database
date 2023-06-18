c88fbf853095aa05db4d0b774542b2c1fac616a6
Szymon Malec <77552397+Szymex49@users.noreply.github.com>
2023-06-18 23:21:02 +0200

    update

--- Szymon/projekt.sql
+++ Szymon/projekt.sql
@@ -4,8 +4,6 @@
 select * from games_for_sale;
 
 
-
-
 drop table competition_results;
 drop table competition;
 drop table games_for_sale;
@@ -14,3 +12,18 @@ drop table staff;
 drop table customers;
 drop table sale;
 drop table rental;
+
+-------------------------------------------------
+
+
+select staff_id, count(*)
+from staff
+left join sale
+  on staff.staff_id = sale.staff_id
+group by staff_id;
+
+left join rental
+  on staff.staff_id = rental.staff_id;
+
+
+
